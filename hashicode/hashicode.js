fb3896bd80704f3bd2f3d37bb9d2545ea3726074
nando <nando@hex7.com>
2024-01-12 15:59:48 +0100

    headers

--- hashicode/hashicode.js
+++ hashicode/hashicode.js
@@ -1,7 +1,18 @@
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
   let resp = '';
-  resp += '<center><h1>HashiCODE BCN</h1><p><h3>HashiCorp DevOps Consultancy.</h3><h3>On-Site Barcelona.</h3>';
+  resp += '<!DOCTYPE html><html lang="en">';
+  resp += '<head><meta charset="UTF-8">';
+  resp += '<meta name="description" content="HashiCorp DevOps Consultants Barcelona.">';
+  resp += '<meta name="viewport" content="width=800; initial-scale=0.4">';
+  resp += '<meta name="author" content="hashicode.hex7.com">';
+  resp += '<meta name="keywords" content="hashicorp, devops, consultant, linux, cloud, terraform">';
+  resp += '<meta http-equiv="refresh" content="10">';
+  resp += '<meta http-equiv="Accept-Encoding" content="gzip; compress; br">';
+  resp += '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+  resp += '<title>HashiCODE BCN :: hashicode.hex7.com</title></head>';
+  resp += '<body><center><h1>HashiCODE BCN</h1><p><h3>HashiCorp DevOps Consultancy.</h3>';
+  resp += '<h3>On-Site Barcelona.</h3>';
   resp += 'Terraform Infrastructure as Code. (DRY Module Support).<p>';
   resp += 'Consul/Nomad Docker Container Clusters. (Faster/Simpler Kubernetes).<p>';
   resp += 'Vault Secrets Engine. (Zero Trust and Secrets Rotation).<p>';
@@ -14,7 +25,8 @@ module.exports.handler = async (event) => {
   resp += '<h2>CONTACT: HashiCODE at hex7 . com</h2><p><br><p><br><p>';
   resp += 'Site Powered by:';
   resp += '<h5>HashiCorp Terraform<br>AWS Serverless Lambda/APIGateway<br>';
-  resp += '<a href=https://github.com/nand0p/hashicode>https://github.com/nand0p/hashicode</a>';
+  resp += '<a href=https://github.com/nand0p/hashicode>https://github.com/nand0p/hashicode</a><br>';
+  resp += '<a href=https://hashicode.hex7.com>https://hashicode.hex7.com</a><br>';
   resp += '<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>.<p><br>';
   resp += JSON.stringify(event.headers);
   resp += '<br>';
@@ -44,6 +56,7 @@ module.exports.handler = async (event) => {
   resp += JSON.stringify(envVars);
   resp += '<br>';
   resp += JSON.stringify(event.requestContext);
+  resp += '</body>';
 
 
   return {
